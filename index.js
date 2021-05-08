182ad9202837127b53235935a5464b77c6e8620a
Coder Hashtag <48601027+coderhashtag03@users.noreply.github.com>
2021-05-08 13:03:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+if (document.getElementById("download_button").disabled === true) {
+  document.getElementById("download_id").href = "javascript: void(0)";
+}
 
+document.getElementById("spotify").onclick = function() {enableSoundCloud()};
+
+function enableSoundCloud() {
+  document.getElementById("soundcloud").disabled = false;
+}
+
+document.getElementById("soundcloud").onclick = function() {enableYoutube()};
+
+function enableYoutube() {
+  document.getElementById("youtube").disabled = false;
+}
+
+document.getElementById("youtube").onclick = function() {enableDownload()};
+
+function enableDownload() {
+  document.getElementById("download_button").disabled = false;
+}
